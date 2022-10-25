9c81419c79b4eca8be540e3f6265d7709b973ee8
Hwaipy Li <hwaipy@gmail.com>
2022-10-25 07:11:33 +0000

    change default port

--- .dockerConfig/start.py
+++ .dockerConfig/start.py
@@ -1,21 +1,18 @@
-from interactionfreepy import IFBroker
-from interactionfreepy import IFLoop
-from interactionfreepy import IFDefinition
 import os
+from interactionfreepy import IFBroker, IFLoop, IFDefinition
 
 broker = IFBroker()
-print('InteractionFree server start at port {}'.format(broker.address[2]))
+print(f'InteractionFree server start at port {broker.address[2]}')
 
 certFiles = {
     "certfile": "/root/.config/ssl/server.crt",
     "keyfile": "/root/.config/ssl/server.key",
 }
-useSSL = os.path.exists(certFiles['certfile']) and os.path.exists(certFiles['keyfile'])
-if useSSL:
-    broker.startWebSocket(IFDefinition.DEFAULT_PORT_WEBSOCKET_SSL, '/wss/', certFiles)
-    print('InteractionFree WebSocket server (SSL) start at port {}, /wss/'.format(IFDefinition.DEFAULT_PORT_WEBSOCKET_SSL))
+if os.path.exists(certFiles['certfile']) and os.path.exists(certFiles['keyfile']):
+  broker.startWebSocket(IFDefinition.DEFAULT_PORT_WEBSOCKET_SSL, '/wss/', certFiles)
+  print('InteractionFree WebSocket server (SSL) start at port {IFDefinition.DEFAULT_PORT_WEBSOCKET_SSL}, /wss/')
 else:
-    broker.startWebSocket(IFDefinition.DEFAULT_PORT_WEBSOCKET, '/ws/')
-    print('InteractionFree WebSocket server (no SSL) start at port {}, /ws/'.format(IFDefinition.DEFAULT_PORT_WEBSOCKET))
+  broker.startWebSocket(IFDefinition.DEFAULT_PORT_WEBSOCKET, '/ws/')
+  print('InteractionFree WebSocket server (no SSL) start at port {IFDefinition.DEFAULT_PORT_WEBSOCKET}, /ws/')
 
 IFLoop.join()
